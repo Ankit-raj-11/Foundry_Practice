cd5b12942b12888e0c55c27c0ae77d4d38b80928
Ankit-raj-11 <ankit74850raj@gmail.com>
2026-06-10 13:37:58 +0530

    test: fix the fourge build issue of evenlogger.t.sol

--- test/eventlogger.t.sol
+++ test/eventlogger.t.sol
@@ -1,16 +1,21 @@
-// SPDX-License-Identifier: UNLICENSEDd
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
-// import {Eventlogger} from "../src/eventlogger.sol";
+import {Eventlogger} from "../src/eventlogger.sol";
 
 contract EventLoggerTest is Test {
-    // Eventlogger public logger;
-    // event NewAnnouncement(uint indexed announcementId, address indexed announcer, string message);
+    Eventlogger public logger;
+    event NewAnnouncement(uint indexed announcementId, address indexed announcer, string message);
+
     function setUp() public {
-        // logger = new Eventlogger();
+        logger = new Eventlogger();
     }
-    function test_bro
 
-
+    function test_broadcast() public {
+        vm.expectEmit(true, true, false, true);
+        emit NewAnnouncement(1, address(this), "hello");
+        logger.broadcast("hello");
+        assertEq(logger.totalAnnouncement(), 1);
+    }
 }
